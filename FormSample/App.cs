a127a4dce7803242f30353c95f1d67a30fc8133c
Sudhir <san.thanki@gmail.com>
2014-11-26 21:42:04 +0530

    Xamarin forms with Sqlite
    Xamarin forms with Sqlite

---
+++ FormSample/App.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Labs.Services;
+
+namespace FormSample
+{
+    using FormSample.Helpers;
+    using FormSample.Views;
+
+    public class App
+    {
+
+        public static INavigation Navigation { get; private set; }
+
+		static void CreateDatabase ()
+		{
+			FormSample.AgentDatabase d = new AgentDatabase ();
+			var t = d.GetAgents ();
+			d.SaveItem (new Agent () {
+				AgencyName = "test",
+				Email = "abc@xyz.com",
+				FirstName = "Thanki",
+				LastName = "sudhir",
+				Phone = "99933423",
+			});
+		}      
+
+        public static Page GetMainPage()
+        {
+			NavigationPage page = null;
+            try
+            {
+				// Settings.GeneralSettings = "hello";
+				if(!string.IsNullOrWhiteSpace(Settings.GeneralSettings))
+				{
+				}
+				CreateDatabase ();
+				page=  new NavigationPage(new RegisterPage());
+
+//			    page = new NavigationPage(new LoginPage());
+//                if (!string.IsNullOrWhiteSpace(Settings.GeneralSettings))
+//                {
+//                    Navigation = page.Navigation;
+//                    // return page;
+//                    // return new HomePage();
+//                }
+//                    // return new LoginPage();
+//                else
+//                {
+//                    page = new NavigationPage(new HomePage());
+//                    Navigation = page.Navigation;
+//                }
+//                // return page;
+//
+//                //var navigationPage = new NavigationPage(new HackerNewsPage());
+//                //Navigation = navigationPage.Navigation;
+//
+//                //return navigationPage;
+//
+//                ////            return new ContentPage
+//                ////            { 
+//                ////                Content = new Label
+//                ////                {
+//                ////                    Text = "Hello, Forms!",
+//                ////                    VerticalOptions = LayoutOptions.CenterAndExpand,
+//                ////                    HorizontalOptions = LayoutOptions.CenterAndExpand,
+//                ////                },
+//                ////            };
+            }
+            catch (Exception ex)
+            {
+            }
+            return page;
+        }
+    }
+
+    public class HackerNewsPage : ContentPage
+    {
+
+        private ListView listView;
+        public HackerNewsPage()
+        {
+            Title = "Customers";
+
+            listView = new ListView
+            {
+                RowHeight = 80
+            };
+
+            Content = new StackLayout
+            {
+                VerticalOptions = LayoutOptions.FillAndExpand,
+                Children = { listView }
+            };
+        }
+
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+
+            var entries = await new DataService().GetCustomers();
+            listView.ItemTemplate = new DataTemplate(typeof(CustomCell));
+            listView.ItemsSource = entries;
+        }
+    }
+}
+
---
+++ FormSample/Model/Customer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using SQLite.Net.Attributes;
+
+namespace FormSample
+{
+
+    public class User
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+    }
+
+    public class Agent
+    {
+		[PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string AgencyName { get; set; }
+        public string Phone { get; set; }
+        
+    }
+
+    public class Contractor
+    {
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public string AdditionalInformation { get; set; }
+    }
+}
+
