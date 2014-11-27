65bd2f786f8383d91358a97c255dbbb90d62fa4e
softsan <san.thanki@gmail.com>
2014-11-27 19:01:15 +0530

    change some code
    change some code

--- FormSample/App.cs
+++ FormSample/App.cs
@@ -4,68 +4,84 @@ using Xamarin.Forms.Labs.Services;
 
 namespace FormSample
 {
+    using System.Threading.Tasks;
+
     using FormSample.Helpers;
     using FormSample.Views;
 
+    using Xamarin.Forms.Labs;
+
     public class App
     {
-
         public static INavigation Navigation { get; private set; }
 
-		static void CreateDatabase ()
-		{
-			FormSample.AgentDatabase d = new AgentDatabase ();
-			var t = d.GetAgents ();
-			d.SaveItem (new Agent () {
-				AgencyName = "test",
-				Email = "abc@xyz.com",
-				FirstName = "Thanki",
-				LastName = "sudhir",
-				Phone = "99933423",
-			});
-		}      
+        static void CreateDatabase()
+        {
+            FormSample.AgentDatabase d = new AgentDatabase();
+            var t = d.GetAgents();
+            d.SaveItem(new Agent()
+            {
+                AgencyName = "test",
+                Email = "abc@xyz.com",
+                FirstName = "Thanki",
+                LastName = "sudhir",
+                Phone = "99933423",
+            });
+        }
+
+        private async Task<bool> IsNetworkAvailable()
+        {
+            var network = Resolver.Resolve<IDevice>().Network;
+            //var dev = Resolver.Resolve<IDevice>().PhoneService;
+            //dev.DialNumber("989898989");
+            
+            var isReachable = await network.IsReachable("www.yahoo.com", TimeSpan.FromSeconds(15));
+            return isReachable;
+
+        }
 
         public static Page GetMainPage()
         {
-			NavigationPage page = null;
+            
+            NavigationPage page = null;
             try
             {
-				// Settings.GeneralSettings = "hello";
-				if(!string.IsNullOrWhiteSpace(Settings.GeneralSettings))
-				{
-				}
-				// CreateDatabase ();
-				page=  new NavigationPage(new LoginPage());
+                Settings.GeneralSettings = "hello";
+                if (!string.IsNullOrWhiteSpace(Settings.GeneralSettings))
+                {
+                }
+                // CreateDatabase ();
+                page = new NavigationPage(new LoginPage());
 
-//			    page = new NavigationPage(new LoginPage());
-//                if (!string.IsNullOrWhiteSpace(Settings.GeneralSettings))
-//                {
-//                    Navigation = page.Navigation;
-//                    // return page;
-//                    // return new HomePage();
-//                }
-//                    // return new LoginPage();
-//                else
-//                {
-//                    page = new NavigationPage(new HomePage());
-//                    Navigation = page.Navigation;
-//                }
-//                // return page;
-//
-//                //var navigationPage = new NavigationPage(new HackerNewsPage());
-//                //Navigation = navigationPage.Navigation;
-//
-//                //return navigationPage;
-//
-//                ////            return new ContentPage
-//                ////            { 
-//                ////                Content = new Label
-//                ////                {
-//                ////                    Text = "Hello, Forms!",
-//                ////                    VerticalOptions = LayoutOptions.CenterAndExpand,
-//                ////                    HorizontalOptions = LayoutOptions.CenterAndExpand,
-//                ////                },
-//                ////            };
+                //			    page = new NavigationPage(new LoginPage());
+                //                if (!string.IsNullOrWhiteSpace(Settings.GeneralSettings))
+                //                {
+                //                    Navigation = page.Navigation;
+                //                    // return page;
+                //                    // return new HomePage();
+                //                }
+                //                    // return new LoginPage();
+                //                else
+                //                {
+                //                    page = new NavigationPage(new HomePage());
+                //                    Navigation = page.Navigation;
+                //                }
+                //                // return page;
+                //
+                //                //var navigationPage = new NavigationPage(new HackerNewsPage());
+                //                //Navigation = navigationPage.Navigation;
+                //
+                //                //return navigationPage;
+                //
+                //                ////            return new ContentPage
+                //                ////            { 
+                //                ////                Content = new Label
+                //                ////                {
+                //                ////                    Text = "Hello, Forms!",
+                //                ////                    VerticalOptions = LayoutOptions.CenterAndExpand,
+                //                ////                    HorizontalOptions = LayoutOptions.CenterAndExpand,
+                //                ////                },
+                //                ////            };
             }
             catch (Exception ex)
             {
